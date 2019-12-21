f97bd41a8984cce86774c2c5b968b57cbcd79845
akunda <1143474942@qq.com>
2019-12-21 13:04:57 +0800

    12.21 final

--- DS2/cmake-build-debug/CMakeFiles/DS2.dir/DependInfo.cmake
+++ DS2/cmake-build-debug/CMakeFiles/DS2.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/gikosei/Desktop/DS+/DS2/main.cpp" "/Users/gikosei/Desktop/DS+/DS2/cmake-build-debug/CMakeFiles/DS2.dir/main.cpp.o"
+  "/Users/akunda/Downloads/DS+/DS2/main.cpp" "/Users/akunda/Downloads/DS+/DS2/cmake-build-debug/CMakeFiles/DS2.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
--- DS2/cmake-build-debug/cmake_install.cmake
+++ DS2/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/gikosei/Desktop/DS+/DS2
+# Install script for directory: /Users/akunda/Downloads/DS+/DS2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/gikosei/Desktop/DS+/DS2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/akunda/Downloads/DS+/DS2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
